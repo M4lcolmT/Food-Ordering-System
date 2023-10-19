cef4437b369ceb8be3e0e0a3b4ca23d1f9814802
M4lcolmT <twhan06@gmail.com>
2023-10-19 10:20:58 +0800

    renamed order history

--- src/food/ordering/system/CustomerGUI/OrderSummary.java
+++ src/food/ordering/system/CustomerGUI/OrderSummary.java
@@ -8,12 +8,12 @@ package food.ordering.system.CustomerGUI;
  *
  * @author jiasi
  */
-public class OrderHistory extends javax.swing.JFrame {
+public class OrderSummary extends javax.swing.JFrame {
 
     /**
      * Creates new form OrderHistory
      */
-    public OrderHistory() {
+    public OrderSummary() {
         initComponents();
     }
 
@@ -258,20 +258,21 @@ public class OrderHistory extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(OrderHistory.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(OrderSummary.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(OrderHistory.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(OrderSummary.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(OrderHistory.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(OrderSummary.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(OrderHistory.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(OrderSummary.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new OrderHistory().setVisible(true);
+                new OrderSummary().setVisible(true);
             }
         });
     }
